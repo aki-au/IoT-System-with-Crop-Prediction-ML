63844c443037d75a16ba0e07cee6641a205c4087
Alakarthika <83805418+aki-au@users.noreply.github.com>
2021-05-21 06:14:04 +0530

    Update croppred_k_nearest_neighbors.py

--- croppred_k_nearest_neighbors.py
+++ croppred_k_nearest_neighbors.py
@@ -100,20 +100,7 @@ crops=['wheat','mungbean','Tea','millet','maize','lentil','jute','cofee','cotton
 cr='rice'
 
 #Predicting the crop
-predictions = classifier.predict(predictcrop)
-count=0
-for i in range(0,30):
-    if(predictions[0][i]==1):
-        c=crops[i]
-        count=count+1
-        break;
-    i=i+1
-if(count==0):
-    print('The predicted crop is %s'%cr)
-else:
-    print('The predicted crop is %s'%c)
 
-"""## Visualising the Test set results"""
 
 predictions = classifier.predict(predictcrop)
 
